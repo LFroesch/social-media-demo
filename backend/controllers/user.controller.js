70d4bb32471e62cf0d1266988b786d71bf8cc530
LFroesch <lucas.froeschner@gmail.com>
2025-06-03 15:54:25 -0700

    follow/unfollow hook

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -53,7 +53,6 @@ export const followUnfollowUser = async (req, res) => {
                 to: userToModify._id,
             });
             await newNotification.save();
-            // TODO: return the id of the user as a response
             res.status(200).json({ message: "Followed successfully" });
         }
     } catch (error) {
